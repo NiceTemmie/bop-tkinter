488a49507bba088c6427d257cc8296b57a4718ba
SomeFrenchGuy <62616818+SomeFrenchGuy@users.noreply.github.com>
2020-06-17 17:11:38 +0200

    Add files via upload

---
+++ games/JustAExemple/exemple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*-coding:Utf-8 -*
+from tkinter import *
+root=Tk()
+Label(root,text='THIS IS GAME BELIEVE ME PLEASE').pack()
+root.mainloop()
---
+++ games/JustAExemple/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+path=$(dirname "$0")
+path+='/exemple.py'
+python3 $path;$SHELL
